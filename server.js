82cdca026d38a67d88e6a19e1f911d472393fdd2
Natalie Ruiz <email": null>
2017-06-05 20:23:59 -0400

    before

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ io.sockets.on('connection', function(socket){
 
   // disconnect
   socket.on('disconnect', function(data){
+    // if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
     console.log("disconnected: %s sockets connected", connections.length);
   });
@@ -27,5 +30,18 @@ io.sockets.on('connection', function(socket){
   socket.on('send message', function(data){
     console.log(data)
     io.sockets.emit('new message', {msg: data});
-  })
+  });
+
+  // new users
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+
+  function updateUsernames(){
+    io.sockets.emit('get users', users);
+  }
 });
